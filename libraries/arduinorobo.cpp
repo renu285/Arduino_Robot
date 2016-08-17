af22a6d63cd3a5cee865745f0a55d8d184156ad6
Pranav Sailesh <pranav656@gmail.com>
2016-08-18 01:03:34 +0200

    Update libraries

--- libraries/arduinorobo.cpp
+++ libraries/arduinorobo.cpp
@@ -1,5 +1,5 @@
 #include "Arduino.h"
-#include "locomotion.h"
+#include "arduinorobo.h"
 
 ultrasonic::udistance()
 {
@@ -13,3 +13,140 @@ ultrasonic::udistance()
        return uldistance;
     
 }
+locomotion::move_front()
+{
+	unsigned long currentMillis = millis();
+    
+        if(locoState!=0 && (currentMillis-previousMillis) >= 1000 && act==1)
+        {
+         locoState=0;
+         pending1=0;
+         act=0;
+         previousMillis = currentMillis;
+         digitalWrite(LMOTOR_P,LOW);
+         digitalWrite(LMOTOR_N,LOW);
+         digitalWrite(RMOTOR_P,LOW);
+         digitalWrite(RMOTOR_N,LOW);
+        }
+    else if(locoState==0 && (currentMillis-previousMillis)<1000 && act==0)
+    {
+      //previousMillis = currentMillis;
+      locoState = 1;
+      pending1=1;
+      act=1;
+      digitalWrite(LMOTOR_P,HIGH);
+      digitalWrite(LMOTOR_N,LOW);
+      digitalWrite(RMOTOR_P,HIGH);
+      digitalWrite(RMOTOR_N,LOW);
+    }
+    else if(locoState!=0)
+    {
+      pending1=1;
+    }
+}
+
+locomotion::move_back()
+  {
+    unsigned long currentMillis = millis();
+    pending2=1;
+    if(locoState!=0 && (currentMillis-previousMillis) >= 2000 && act==2)
+    {
+      locoState=0;
+      pending2=0;
+      act=0;
+      previousMillis = currentMillis;
+      digitalWrite(LMOTOR_P,LOW);
+      digitalWrite(LMOTOR_N,LOW);
+      digitalWrite(RMOTOR_P,LOW);
+      digitalWrite(RMOTOR_N,LOW);
+    }
+    else if(locoState==0 && (currentMillis-previousMillis)<2000 && act==0)
+    {
+      //previousMillis = currentMillis;
+      locoState = 1;
+      pending2=1;
+      act=2;
+      digitalWrite(LMOTOR_P,LOW);
+      digitalWrite(LMOTOR_N,HIGH);
+      digitalWrite(RMOTOR_P,LOW);
+      digitalWrite(RMOTOR_N,HIGH);
+    }
+    else if(locoState!=0)
+    {
+      pending2=1;
+    }
+  }
+locomotion::move_right()
+  {
+    unsigned long currentMillis = millis();
+    //pending2=1;
+    if(locoState!=0 && (currentMillis-previousMillis) >= 2000 && act==3)
+    {
+      locoState=0;
+      pending2=0;
+      act=0;
+      previousMillis = currentMillis;
+      digitalWrite(LMOTOR_P,LOW);
+      digitalWrite(LMOTOR_N,LOW);
+      digitalWrite(RMOTOR_P,LOW);
+      digitalWrite(RMOTOR_N,LOW);
+    }
+    else if(locoState==0 && (currentMillis-previousMillis)<2000 && act==0)
+    {
+      //previousMillis = currentMillis;
+      locoState = 1;
+      //pending2=1;
+      act=3;
+      digitalWrite(LMOTOR_P,LOW);
+      digitalWrite(LMOTOR_N,LOW);
+      digitalWrite(RMOTOR_P,HIGH);
+      digitalWrite(RMOTOR_N,LOW);
+    }
+    else if(locoState!=0)
+    {
+      pending2=1;
+    }
+  }
+ locomotion::move_left()
+  {
+    unsigned long currentMillis = millis();
+    //pending2=1;
+    if(locoState!=0 && (currentMillis-previousMillis) >= 1000 && act==4)
+    {
+      locoState=0;
+      pending2=0;
+      act=0;
+      previousMillis = currentMillis;
+      digitalWrite(LMOTOR_P,LOW);
+      digitalWrite(LMOTOR_N,LOW);
+      digitalWrite(RMOTOR_P,LOW);
+      digitalWrite(RMOTOR_N,LOW);
+    }
+    else if(locoState==0 && (currentMillis-previousMillis)<1000 && act==0)
+    {
+      //previousMillis = currentMillis;
+      locoState = 1;
+      //pending2=1;
+      act=4;
+      digitalWrite(LMOTOR_P,HIGH);
+      digitalWrite(LMOTOR_N,LOW);
+      digitalWrite(RMOTOR_P,LOW);
+      digitalWrite(RMOTOR_N,LOW);
+    }
+    else if(locoState!=0)
+    {
+      pending2=1;
+    }
+  }
+
+
+  
+locomotion:: init()
+  {
+    pinMode(LMOTOR_P,OUTPUT);
+    pinMode(LMOTOR_N,OUTPUT);
+    pinMode(RMOTOR_N,OUTPUT);
+    pinMode(RMOTOR_P,OUTPUT);
+    
+  }
+
--- libraries/arduinorobo.h
+++ libraries/arduinorobo.h
@@ -22,6 +22,35 @@ class ultrasonic
   }
   float udistance();
 };
-#endif
+
+class locomotion
+{
+  int locoState; //variable to store the locomotion state
+  int pending1,pending2;
+  unsigned long previousMillis;
+  int act;
+  int LMOTOR_P,LMOTOR_N,RMOTOR_P,RMOTOR_N;
+  public:
+  locomotion(int LP,int LN,int RP,int RN)
+  {
+	
+    locoState=0;
+    previousMillis = 0;
+    pending1=0;
+    pending2=0;
+    LMOTOR_P=LP;
+    RMOTOR_P=RP;
+    LMOTOR_N=LN;
+    RMOTOR_N=RN;
+  }
+  void move_front();
+  void move_back();
+  void move_left();
+  void move_right();
+  void move_left();
+  void init();
+};
+
+
 
 #endif
